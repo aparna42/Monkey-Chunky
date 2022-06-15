e43cd65d3adc735413515669f88de4a5b19d41bf
aparna42 <32508791+aparna42@users.noreply.github.com>
2022-06-16 03:33:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default class App extends React.Component {
     this.state = {
       text: '',
       chunks: [],
-      phonicSounds: [],
+      phonicSounds: []
     };
   }
   render() {
